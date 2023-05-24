d7c672b707c3edf5bfc3202032436e8165e82f9e
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-05-24 10:23:36 -0300

    adjusts in auth and jwt

---
+++ src/main/java/med/voll/api/auth/controller/AutenticacaoController.java
@@ -0,0 +1,50 @@
+package med.voll.api.auth.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.auth.service.TokenService;
+import med.voll.api.auth.usuario.DTO.DadosAutenticacaoDTO;
+import med.voll.api.auth.usuario.domain.Usuario;
+import med.voll.api.config.security.DadosTokenJWT;
+import med.voll.api.exceptions.NoTokenException;
+import med.voll.api.exceptions.TokenInvalidoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacaoController {
+
+	@Autowired
+	private AuthenticationManager manager;
+
+	@Autowired
+	private TokenService tokenService; // token da nossa aplicação
+
+	@PostMapping
+	public ResponseEntity<?> efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dados) {
+		try {
+			var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+			var authentication = manager.authenticate(authenticationToken);
+			var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+			var userDoToken = dados.login();
+			var expiracao = tokenService.getExpiration(tokenJWT);
+			return ResponseEntity.ok(new DadosTokenJWT(tokenJWT, userDoToken, expiracao));
+		} catch (TokenInvalidoException e) {
+			throw new NoTokenException(HttpStatus.FORBIDDEN, "Token inválido ou expirado!");
+		} catch (AuthenticationException e) {
+			var responseError = Map.of("error", "Falha na autenticação");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseError);
+		}
+	}
+
+}
