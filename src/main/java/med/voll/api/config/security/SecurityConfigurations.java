d0f7a982e1cd300ac1d7ef1bde415141c7022ecb
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-06-01 18:30:07 -0300

    adjusts

--- src/main/java/med/voll/api/config/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/config/security/SecurityConfigurations.java
@@ -55,7 +55,7 @@ public class SecurityConfigurations {
 	}
 	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
