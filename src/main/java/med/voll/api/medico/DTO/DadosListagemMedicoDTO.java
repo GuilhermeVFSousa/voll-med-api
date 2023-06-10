7b5f9820fc6bf0a06c63e8dabacfc37a25ed67be
GuilhermeVFSousa <guilhermevfsousa@gmail.com>
2023-06-10 09:30:39 -0300

    add telefone to DadosListagemMedicoDTO

--- src/main/java/med/voll/api/medico/DTO/DadosListagemMedicoDTO.java
+++ src/main/java/med/voll/api/medico/DTO/DadosListagemMedicoDTO.java
@@ -5,16 +5,18 @@ import med.voll.api.medico.domain.Medico;
 
 public record DadosListagemMedicoDTO(
 		Long id,
-		String nome, 
-		String email, 
-		String crm, 
+		String nome,
+		String email,
+		String telefone,
+		String crm,
 		Especialidade especialidade
-		) {
-	
+) {
+
 	public DadosListagemMedicoDTO(Medico medico) {
 		this(
 				medico.getId(),
 				medico.getNome(),
+				medico.getEmail(),
 				medico.getEmail(),
 				medico.getCrm(),
 				medico.getEspecialidade());
